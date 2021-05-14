fbe11667decbd0a94fc34914a73c768b824ea69f
Nabanita <nroy0110@gmail.com>
2021-05-14 23:44:12 +0100

    Adding comments

--- common/helper.py
+++ common/helper.py
@@ -13,12 +13,14 @@ from common.common_settings import CommonConfig
 
 class Helper:
     @staticmethod
-    def get_file_name(file_name):
+    def generate_file_name(file_name):
+        """ Generates a file name to store the results"""
         return file_name + '_' + str(datetime.now().strftime("%d_%m_%Y")) + '_' + str(datetime.now().strftime("%H_%M_%S"))
 
     @staticmethod
     def write_to_excel(df, file_name):
-        formatted_file_name = Helper.get_file_name(file_name)
+        """ Writes the results to an excel sheet """
+        formatted_file_name = Helper.generate_file_name(file_name)
         writer = ExcelWriter(CommonConfig.DATA_FOLDER_PATH + '/' + formatted_file_name + '.xlsx')
         df.to_excel(writer)
         writer.save()
@@ -26,18 +28,21 @@ class Helper:
 
     @staticmethod
     def write_to_pickle(df, file_name):
-        formatted_file_name = Helper.get_file_name(file_name)
+        """ Writes the results to a pickle file"""
+        formatted_file_name = Helper.generate_file_name(file_name)
         df.to_pickle(CommonConfig.DATA_FOLDER_PATH + '/' + formatted_file_name + '.pkl')
         print("-------------WRITING TO PICKLE COMPLETED-------------")
 
     @staticmethod
     def read_from_pickle(file_name):
+        """ Reads from a pickle file"""
         df = pd.read_pickle(file_name)
         print("-------------READ FROM PICKLE COMPLETED-------------")
         return df
 
     @staticmethod
     def remove_type_columns(df):
+        """ Removes all data type columns"""
         cols_to_remove = []
         for column in df.columns:
             if "type" in column:
@@ -47,6 +52,7 @@ class Helper:
 
     @staticmethod
     def create_data_folder(path):
+        """ To create a new folder to store query results """
         try:
             if not os.path.isdir(path):
                 os.mkdir(path)
